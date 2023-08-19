7245d852b8121b0bcd93dbae3cf521be5df97258
Lucas Pires Camargo <lucas@camargo.eng.br>
2023-08-20 00:52:35 +0200

    [art] added editor-only tileset for object markers

--- art/blockset/field_a.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="field_a" tilewidth="16" tileheight="16" tilecount="280" columns="20">
- <image source="../tileset_proto.xcf" trans="ff00ff" width="320" height="224"/>
-</tileset>
